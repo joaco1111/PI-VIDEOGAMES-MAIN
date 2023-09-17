b98bdfe26cde50ebc196145864e1837b11d2377e
joaco1111 <joacoo973@gmail.com>
2023-09-17 20:10:16 -0300

    logica de respuestas

--- api/src/handlers/handlers.js
+++ api/src/handlers/handlers.js
@@ -1,50 +1,97 @@
-const { Videogame, Genre } = require('./models'); // Importa los modelos de la base de datos.
+const { Videogame, Genre } = require('./models'); // tengo que importar los modelos de la base de datos
 
-// Manejadores de rutas relacionadas con videojuegos
+// Rutas
 const getVideogames = async (req, res) => {
   try {
-    // Lógica para obtener una lista de videojuegos, ya sea de la base de datos o de la API externa.
-    // Puedes utilizar Sequelize para interactuar con la base de datos.
-    // Luego, envía la respuesta al cliente.
-    res.json(/* Datos de videojuegos obtenidos */);
-  } catch (error) {
-    console.error('Error al obtener videojuegos:', error);
-    res.status(500).json({ message: 'Error en el servidor' });
-  }
+        const localVideogames = await Videogame.findAll();
+    
+        const apiResponse = await axios.get('https://api.rawg.io/api/games', {
+          params: {
+            api_key: '0b7fb8c0aba047d3be896c3c0614c0c7', },
+        });
+
+        const apiVideogames = apiResponse.data.results;
+        const combinedVideogames = [...localVideogames, ...apiVideogames];
+        res.json(combinedVideogames);
+
+      } catch (error) {
+        console.error('Error al obtener videojuegos:', error);
+        res.status(500).json({ message: 'Error en el servidor' });
+      }
 };
+  
+
+const axios = require('axios'); // Importa axios para hacer solicitudes HTTP a la API externa.
 
 const getVideogameById = async (req, res) => {
   try {
     const { idVideogame } = req.params;
-    // Lógica para obtener los detalles de un videojuego específico por su ID.
-    // Utiliza Sequelize para buscar en la base de datos o realiza una solicitud a la API externa.
-    // Luego, envía la respuesta al cliente.
-    res.json(/* Detalles del videojuego */);
+
+    // (obtener el videojuego de la base de datos con Sequelize):
+    const localVideogame = await Videogame.findByPk(idVideogame);
+    if (localVideogame) {
+      res.json(localVideogame);
+    } else {
+      const apiResponse = await axios.get(`https://api.rawg.io/api/games/${idVideogame}`, {
+        params: {
+          api_key: '0b7fb8c0aba047d3be896c3c0614c0c7', },
+      });
+      const apiVideogame = apiResponse.data;
+      res.json(apiVideogame);
+    }
   } catch (error) {
     console.error('Error al obtener el detalle del videojuego:', error);
     res.status(500).json({ message: 'Error en el servidor' });
   }
 };
 
+
+
 const createVideogame = async (req, res) => {
   try {
-    // Lógica para crear un nuevo videojuego utilizando los datos enviados en el cuerpo de la solicitud.
-    // Asegúrate de validar y procesar los datos correctamente.
-    // Luego, envía la respuesta al cliente.
-    res.json(/* Respuesta de creación exitosa */);
+    const { name, image, description, platforms, releaseDate, rating, genres } = req.body;
+    const newVideogame = await Videogame.create({
+      name,
+      image,
+      description,
+      platforms,
+      releaseDate,
+      rating,
+    });
+
+    if (genres && genres.length > 0) {
+      await newVideogame.setGenres(genres);
+    }
+
+    res.status(201).json(newVideogame); // Devuelve el videojuego creado.
   } catch (error) {
     console.error('Error al crear un nuevo videojuego:', error);
     res.status(500).json({ message: 'Error en el servidor' });
   }
 };
 
-// Manejador de ruta relacionada con géneros
+
+
+// Ruta géneros
+const axios = require('axios');
+
 const getGenres = async (req, res) => {
   try {
-    // Lógica para obtener una lista de géneros, ya sea de la base de datos o de la API externa.
-    // Puedes utilizar Sequelize para interactuar con la base de datos.
-    // Luego, envía la respuesta al cliente.
-    res.json(/* Datos de géneros obtenidos */);
+    const localGenres = await Genre.findAll();
+    if (localGenres.length > 0) {
+      res.json(localGenres);
+
+    } else {
+
+      const apiResponse = await axios.get('https://api.rawg.io/api/genres', {
+        params: {
+          api_key: '0b7fb8c0aba047d3be896c3c0614c0c7', },
+      });
+
+      const apiGenres = apiResponse.data.results;
+      res.json(apiGenres);
+
+    }
   } catch (error) {
     console.error('Error al obtener géneros:', error);
     res.status(500).json({ message: 'Error en el servidor' });
