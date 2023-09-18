bc1226d3e6d5d7b17e27acb2be98c3778039a098
joaco1111 <joacoo973@gmail.com>
2023-09-18 16:25:35 -0300

    definicion de el modelo

--- api/src/models/Videogame.js
+++ api/src/models/Videogame.js
@@ -2,11 +2,27 @@ const { DataTypes } = require('sequelize');
 // Exportamos una funcion que define el modelo
 // Luego le injectamos la conexion a sequelize.
 module.exports = (sequelize) => {
-  // defino el modelo
   sequelize.define('videogame', {
     name: {
       type: DataTypes.STRING,
       allowNull: false,
+    },
+    description: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+    platforms: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    image: {
+      type: DataTypes.STRING,
+    },
+    releaseDate: {
+      type: DataTypes.DATE,
+    },
+    rating: {
+      type: DataTypes.FLOAT,
     },
   });
 };
